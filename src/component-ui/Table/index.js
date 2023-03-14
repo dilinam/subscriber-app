acb684b8e2c7492f73ddafa5f7a5c5350d56bb65
hasitha lakmal <hasithalakmal0617@gmail.com>
2023-03-14 14:12:50 +0530

    add material react table & add table component

--- src/component-ui/Table/index.js
+++ src/component-ui/Table/index.js
@@ -1,3 +1,172 @@
-export default function Table() {
-  return <div></div>;
-}
+import React, { useEffect, useMemo, useState } from "react";
+import MaterialReactTable from "material-react-table";
+const data1 = [
+  {
+    name: {
+      firstName: "John",
+      lastName: "Doe",
+    },
+    address: "261 Erdman Ford",
+    city: "East Daphne",
+    state: "Kentucky",
+  },
+  {
+    name: {
+      firstName: "Jane",
+      lastName: "Doe",
+    },
+    address: "769 Dominic Grove",
+    city: "Columbus",
+    state: "Ohio",
+  },
+  {
+    name: {
+      firstName: "Joe",
+      lastName: "Doe",
+    },
+    address: "566 Brakus Inlet",
+    city: "South Linda",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "Kevin",
+      lastName: "Vandy",
+    },
+    address: "722 Emie Stream",
+    city: "Lincoln",
+    state: "Nebraska",
+  },
+  {
+    name: {
+      firstName: "Joshua",
+      lastName: "Rolluffs",
+    },
+    address: "32188 Larkin Turnpike",
+    city: "Charleston",
+    state: "South Carolina",
+  },
+];
+
+const Table = () => {
+  //data and fetching state
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefetching, setIsRefetching] = useState(false);
+  const [rowCount, setRowCount] = useState(0);
+
+  //table state
+  const [globalFilter, setGlobalFilter] = useState("");
+  const [sorting, setSorting] = useState([]);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+
+  //if you want to avoid useEffect, look at the React Query example instead
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!data.length) {
+        setIsLoading(true);
+      } else {
+        setIsRefetching(true);
+      }
+
+      const url = new URL(
+        "/api/data",
+        process.env.NODE_ENV === "production"
+          ? "https://www.material-react-table.com"
+          : "http://localhost:3000"
+      );
+      url.searchParams.set(
+        "start",
+        `${pagination.pageIndex * pagination.pageSize}`
+      );
+      url.searchParams.set("size", `${pagination.pageSize}`);
+      url.searchParams.set("globalFilter", globalFilter ?? "");
+      url.searchParams.set("sorting", JSON.stringify(sorting ?? []));
+
+      try {
+        const response = await fetch(url.href);
+        const json = await response.json();
+        // setData(json.data);
+        setData(data)
+        setRowCount(json.meta.totalRowCount);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+        return;
+      }
+      setIsError(false);
+      setIsLoading(false);
+      setIsRefetching(false);
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    globalFilter,
+    pagination.pageIndex,
+    pagination.pageSize,
+    sorting,
+  ]);
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "name.firstName", //access nested data with dot notation
+        header: "First Name",
+      },
+      {
+        accessorKey: "name.lastName",
+        header: "Last Name",
+      },
+      {
+        accessorKey: "address", //normal accessorKey
+        header: "Address",
+      },
+      {
+        accessorKey: "city",
+        header: "City",
+      },
+      {
+        accessorKey: "state",
+        header: "State",
+      },
+    ],
+    []
+  );
+
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={data1}
+      enableRowSelection
+      getRowId={(row) => row.phoneNumber}
+      manualPagination
+      manualSorting
+      muiToolbarAlertBannerProps={
+        isError
+          ? {
+              color: "error",
+              children: "Error loading data",
+            }
+          : undefined
+      }
+      onGlobalFilterChange={setGlobalFilter}
+      onPaginationChange={setPagination}
+      onSortingChange={setSorting}
+      rowCount={rowCount}
+      state={{
+        globalFilter,
+        // isLoading,
+        pagination,
+        // showAlertBanner: isError,
+        // showProgressBars: isRefetching,
+        sorting,
+      }}
+    />
+  );
+};
+
+export default Table;
