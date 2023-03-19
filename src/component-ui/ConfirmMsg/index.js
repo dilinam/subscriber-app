00ef251fda425c96255d5a1937e53034970dd554
hasitha lakmal <hasithalakmal0617@gmail.com>
2023-03-19 20:12:26 +0530

    change modal to dialog in cofirmmsg

--- src/component-ui/ConfirmMsg/index.js
+++ src/component-ui/ConfirmMsg/index.js
@@ -1,53 +1,49 @@
 import React from 'react'
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "60%",
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
 const ConfirmMsg = (props) => {
-     const [open, setOpen] = React.useState(false);
-     const handleOpen = () => setOpen(true);
-     const handleClose = () => setOpen(false);
+      const [open, setOpen] = React.useState(false);
+
+      const handleClickOpen = () => {
+        setOpen(true);
+      };
+
+      const handleClose = () => {
+        setOpen(false);
+      };
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Slide in alert dialog
+      </Button>
+      <Dialog
         open={open}
+        TransitionComponent={Transition}
+        keepMounted
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        aria-describedby="alert-dialog-slide-description"
       >
-        <Box sx={style}>
-          <Typography
-            id="modal-modal-title"
-            variant="h6"
-            component="h2"
-            sx={{ mt: 2, color: "white" }}
-          >
-            {props.title}
-          </Typography>
-          <Typography
-            id="modal-modal-description"
-            sx={{ mt: 2, color: "white" }}
-          >
+        <DialogTitle>{props.title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
             {props.msg}
-          </Typography>
-          <Button sx={{ mt: 2 }}> Confirm</Button>
-          <Button sx={{ mt: 2, float: "right" }}> Cancel</Button>
-        </Box>
-      </Modal>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose}>Agree</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
