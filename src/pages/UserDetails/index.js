113b5307e8d8c05a1d1e32191cbc38ca7f3e9dcf
hasitha lakmal <hasithalakmal0617@gmail.com>
2023-05-23 00:36:02 +0530

    card magmt and update profile

--- src/pages/UserDetails/index.js
+++ src/pages/UserDetails/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Button,
@@ -7,134 +7,86 @@ import {
   CardContent,
   CardHeader,
   TextField,
-  Unstable_Grid2 as Grid} from "@mui/material";
-
-const states = [
-  {
-    value: "alabama",
-    label: "Alabama",
-  },
-  {
-    value: "new-york",
-    label: "New York",
-  },
-  {
-    value: "san-francisco",
-    label: "San Francisco",
-  },
-  {
-    value: "los-angeles",
-    label: "Los Angeles",
-  },
-];
+  Unstable_Grid2 as Grid,
+} from "@mui/material";
+import { getUserDetails } from "../../use-cases/get-user-details";
+import { editUser } from "../../use-cases/edit-user";
 
 const UserDetails = () => {
-  const [values, setValues] = React.useState({
-    firstName: "Anika",
-    lastName: "Visser",
-    email: "demo@devias.io",
-    phone: "",
-    state: "los-angeles",
-    country: "USA",
-  });
-
-  const handleChange =((event) => {
-    setValues((prevState) => ({
+  const [user, setUser] = useState({});
+  const handleChange = (event) => {
+    setUser((prevState) => ({
       ...prevState,
       [event.target.name]: event.target.value,
     }));
-  });
-
-  const handleSubmit = ((event) => {
-    event.preventDefault();
+  };
+  useEffect(() => {
+    getUserDetails().then((res) => setUser(res.data));
   }, []);
 
+  // const handleSubmit =
+  //   ((event) => {
+  //     event.preventDefault();
+  //   },
+  //   []);
+console.log(user);
+  const save = () => {
+    editUser(user);
+    console.log(user)
+  };
+
   return (
-    <form autoComplete="off" noValidate onSubmit={handleSubmit}>
-      <Card>
-        <CardHeader subheader="The information can be edited" title="Profile" />
-        <CardContent sx={{ pt: 0 }}>
-          <Box sx={{ mb:5 }}>
-            <Grid container spacing={3}>
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  helperText="Please specify the first name"
-                  label="First name"
-                  name="firstName"
-                  onChange={handleChange}
-                  required
-                  value={values.firstName}
-                />
-              </Grid>
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Last name"
-                  name="lastName"
-                  onChange={handleChange}
-                  required
-                  value={values.lastName}
-                />
-              </Grid>
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Email Address"
-                  name="email"
-                  onChange={handleChange}
-                  required
-                  value={values.email}
-                />
-              </Grid>
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Phone Number"
-                  name="phone"
-                  onChange={handleChange}
-                  type="number"
-                  value={values.phone}
-                />
-              </Grid>
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Country"
-                  name="country"
-                  onChange={handleChange}
-                  required
-                  value={values.country}
-                />
-              </Grid>
-              <Grid xs={12} md={6}>
-                <TextField
-                  fullWidth
-                  label="Select State"
-                  name="state"
-                  onChange={handleChange}
-                  required
-                  select
-                  SelectProps={{ native: true }}
-                  value={values.state}
-                >
-                  {states.map((option) => (
-                    <option key={option.value} value={option.value}>
-                      {option.label}
-                    </option>
-                  ))}
-                </TextField>
-              </Grid>
-              <Grid xs={12} md={6}>
-                <CardActions>
-                  <Button variant="contained">Save details</Button>
-                </CardActions>
-              </Grid>
+    <Card>
+      <CardHeader subheader="The information can be edited" title="Profile" />
+      <CardContent sx={{ pt: 0 }}>
+        <Box sx={{ mb: 5 }}>
+          <Grid container spacing={3}>
+            <Grid xs={12} md={6}>
+              <TextField
+                fullWidth
+                // label="First name"
+                name="firstName"
+                onChange={handleChange}
+                required
+                value={user.firstName}
+              />
             </Grid>
-          </Box>
-        </CardContent>
-      </Card>
-    </form>
+            <Grid xs={12} md={6}>
+              <TextField
+                fullWidth
+                // label="Last name"
+                name="lastName"
+                onChange={handleChange}
+                required
+                value={user.lastName}
+              />
+            </Grid>
+            <Grid xs={12} md={6}>
+              <TextField
+                fullWidth
+                // label="Email Address"
+                name="email"
+                onChange={handleChange}
+                required
+                value={user.email}
+              />
+            </Grid>
+            <Grid xs={12} md={6}>
+              <CardActions>
+                <Button
+                  variant="contained"
+                  size="medium"
+                  value="50"
+                  onClick={save}
+                >
+                  Save details
+                </Button>
+              </CardActions>
+            </Grid>
+          </Grid>
+        </Box>
+      </CardContent>
+    </Card>
   );
 };
 
